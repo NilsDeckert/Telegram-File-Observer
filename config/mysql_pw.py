1d10e8535d13db0cf3412b2637333e480b0cec2f
Nils Deckert <nils_deckert@web.de>
2020-02-23 20:26:53 +0100

    shifted a comment

--- config/mysql_pw.py
+++ config/mysql_pw.py
@@ -1,6 +1,6 @@
 import mysql.connector
 from mysql.connector import errorcode
-login = mysql.connector.connect(         #MySQL login details
+login = mysql.connector.connect(#MySQL login details
         host="HOST_ADDRESS", #eg localhost
         port="3306",
         user="MYSQL_USER",
